320c030ca0a6c40dd859eb41626947b33f6b4167
Pijussad <64306577+Pijussad@users.noreply.github.com>
2023-10-03 16:50:10 +0300

    Add files via upload

---
+++ generate_files2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Function to generate random string of a given length
+generate_random_string() {
+    LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c "$1"
+}
+
+# Function to create pairs and write to the file
+create_pairs() {
+    local length="$1"
+    local count="$2"
+    local diff_position
+
+    for ((i=1; i<=$count; i++)); do
+        # Adjusted the length of the string
+        string1=$(generate_random_string "$length")
+        
+        # Ensure that the second string differs by only one character
+        diff_position=$((RANDOM % length))
+        string2=$(echo "$string1" | sed "s|.|$(generate_random_string 1)|$(($diff_position + 1))")
+
+
+        echo "$string1" >> output2.txt
+        echo "$string2" >> output2.txt
+    done
+}
+
+# Create pairs for different lengths
+create_pairs 10 25000   # 25,000 pairs with 10 characters
+create_pairs 100 25000  # 25,000 pairs with 100 characters
+create_pairs 500 25000  # 25,000 pairs with 500 characters
+create_pairs 1000 25000 # 25,000 pairs with 1000 characters
+
+echo "File created successfully: output.txt"
