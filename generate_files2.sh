7af112bb0e346ec8e81a6a1217c1c01eebbf4491
Pijussad <64306577+Pijussad@users.noreply.github.com>
2023-10-03 18:42:36 +0300

    Update generate_files2.sh

--- generate_files2.sh
+++ generate_files2.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-# Function to generate random string of a given length
+# Funkcija, kuri sugeneruoja atsitiktinį eilutės ilgio simbolių seką
 generate_random_string() {
     LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c "$1"
 }
 
-# Function to create pairs and write to the file
+# Funkcija, kuri sukuria poras ir įrašo į failą
 create_pairs() {
     local length="$1"
     local count="$2"
     local diff_position
 
     for ((i=1; i<=$count; i++)); do
-        # Adjusted the length of the string
+        # Pritaikyta eilutės ilgio
         string1=$(generate_random_string "$length")
         
-        # Ensure that the second string differs by only one character
+        # Užtikrina, kad antroji eilutė skirtųsi tik vienu simboliu
         diff_position=$((RANDOM % length))
         string2=$(echo "$string1" | sed "s|.|$(generate_random_string 1)|$(($diff_position + 1))")
-
 
         echo "$string1" >> output2.txt
         echo "$string2" >> output2.txt
     done
 }
 
-# Create pairs for different lengths
-create_pairs 10 25000   # 25,000 pairs with 10 characters
-create_pairs 100 25000  # 25,000 pairs with 100 characters
-create_pairs 500 25000  # 25,000 pairs with 500 characters
-create_pairs 1000 25000 # 25,000 pairs with 1000 characters
+# Sukurti poras su skirtingais ilgiais
+create_pairs 10 25000   # 25,000 poros su 10 simboliais
+create_pairs 100 25000  # 25,000 poros su 100 simboliais
+create_pairs 500 25000  # 25,000 poros su 500 simboliais
+create_pairs 1000 25000 # 25,000 poros su 1000 simboliais
 
-echo "File created successfully: output.txt"
+echo "Failas sėkmingai sukurtas: output.txt"
